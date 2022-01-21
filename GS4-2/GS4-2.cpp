604106d951b966af6c21b27d8c2cfa8427b3841a
Jacob Hansen <hansenj@rsd407.org>
2022-01-20 17:25:08 -0800

    add

---
+++ GS4-2/GS4-2.cpp
@@ -0,0 +1,42 @@
+//jacob hansen
+//square root reducer
+
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int rootReducer(int num1)
+{
+	int factor = 1;
+	for (int i = 1; i <= num1; i++)
+	{
+		if (num1 % (i * i) == 0)
+		{
+			factor = i;
+		}
+	}
+	return factor;
+}
+
+int main()
+{
+	int num1;
+	int factor;
+	char sqrtSymbol = 251;
+
+	printf("input the number you want to try to reduce\n");
+	cin >> num1;
+	factor = rootReducer(num1);
+	num1 = num1 / factor;
+	num1 = num1 / factor;
+	system("CLS");
+	cout << factor << sqrtSymbol << num1 << endl;
+	return 0;
+}
