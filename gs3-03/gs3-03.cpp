1283e9f5d16c39c5364d112f1cc07fc36d6ed9c2
Jacob Hansen <hansenj@rsd407.org>
2022-01-20 21:58:39 -0800

    add

---
+++ gs3-03/gs3-03.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    int ans1 = 0;
+    int ans2;
+    int ans3;
+    float score = 0;
+
+    printf("please input your name\n");
+    cin >> name;
+    printf("ok %s, the test will commence shortly\n", name.c_str());
+    printf("-press any button to start-");
+    _getch();
+    system("CLS");
+
+    printf("- please input the code to print a sentence:\n");
+    printf("1. say(hello);\n");
+    printf("2. printf(\"hello\");\n");
+    printf("3. tell=*hello*:\n");
+    printf("4. hmmmmmmmmmmmmmmmmmmm\n");
+    cin >> ans1;
+    switch (ans1)
+    {
+    case 1:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 2:
+        printf("correct\n");
+        score = score + 1;
+        break;
+    case 3:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 4:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    }
+    system("CLS");
+
+    printf("- please input number the code that tells the code that it has ended:\n");
+    printf("1. end;\n 2. stop\n 3. return 0;\n 4. dont'n\n");
+    cin >> ans2;
+    switch (ans2)
+    {
+    case 1:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 2:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 3:
+        printf("correct\n");
+        score = score + 1;
+        break;
+    case 4:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    }
+    system("CLS");
+
+    printf("- please input the number next to the code to change the name of a variable:\n");
+    printf("1. intchange**\n 2. cin >>\n 3. switch,\n 4. convertg;;\n");
+    cin >> ans3;
+    switch (ans3)
+    {
+    case 1:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 2:
+        printf("correct\n");
+        score = score + 1;
+        break;
+    case 3:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    case 4:
+        printf("wrong awnser\n");
+        score = score - 0.25;
+        break;
+    }
+    system("CLS");
+
+    if (score < 0)
+    {
+        printf("nice job bozo you got %f", score);
+    }
+    else if (score > 0)
+    {
+        printf("good job %s, you got %f points", name.c_str(), score);
+    }
+    return 0;
+}
---
+++ gs3-05/gs3-05.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	string art;
+
+
+	printf("welcome to the art museum. we have like 3 pictures for you to look at\n");
+	printf("which would you like to examine\n");
+	printf("hope\ndreams\nstroke\n");
+	cin >> art;
+	if (art == "hope")
+	{
+		printf("\|/    \|/\n");
+		printf("  \    /\n");
+		printf("   \_/  ___   ___   \n");
+		printf(   "o o-'   '''   '  \n");
+		printf("    O -.         |\  \n");
+		printf("       | |'''| |      \n");
+		printf("        ||   | |       \n");
+		printf("        ||    ||       \n");
+		printf("        "     "        \n");
